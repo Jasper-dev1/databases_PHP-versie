9e40b734674e45d05c0c4711a843af9390c1924a
Jasper-dev1 <152420@csgaugustinus.nl>
2022-04-20 08:21:30 +0000

    aa

--- public/PHPlessen/_oefen.php
+++ public/PHPlessen/_oefen.php
@@ -1,4 +1,8 @@
 <?php
 // met dubbele slashes maak je een commentaarregel
 echo 'Hello <strong>world</strong>!!';
+$weer = array('Beheerder'=>'Kees', 'Locatie' => 'Veendam', 'ID' => '123');
+echo '<br>' .$weer['Beheerder'].'is de eigenaar van'.$weer['ID'];
+print_r($weer);
+
 ?>
--- public/PHPlessen/opdracht_2.php
+++ public/PHPlessen/opdracht_2.php
@@ -5,10 +5,14 @@ include('opdracht_begin.php');
 TYP HIERONDER JOUW PHPCODE
 ****************************/
 
-$tekst="Ik heb een bijbaantje bij de Aldi. De Aldi betaalt goed.";
-echo "$tekst";
-//$tekst=str_replace('Aldi','Albert Heijn',$tekst);
-echo '<h4>$tekst</h4>';
+$tekst="Ik heb eenx bijbaantje bij de Aldi. De Aldi betaalt goed.";
+echo $tekst;
+$tekst=str_replace('Aldi','Albert Heijn',$tekst);
+echo '<h4> ';
+echo $tekst;
+echo '</h4>';
+echo '<h3>'.$tekst.'</h3>';
+echo "<h2>$tekst</h2>";
 
 /****************************
 EINDE VAN JOUW PHPCODE
